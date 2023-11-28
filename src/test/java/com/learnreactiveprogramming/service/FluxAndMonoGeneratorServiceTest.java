1ae4725ca3503da123682be85e06abae67360bc0
Justin Large <justin.large@keap.com>
2023-11-28 06:59:06 -0700

    Section 7

--- src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,4 +1,23 @@
 package com.learnreactiveprogramming.service;
 
-public class FluxAndMonoGeneratorServiceTest {
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+class FluxAndMonoGeneratorServiceTest {
+
+    private final FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+        StepVerifier.create(fluxAndMonoGeneratorService.namesFlux())
+                .expectNext("alex", "ben", "chloe")
+                .verifyComplete();
+    }
+
+    @Test
+    void nameMono() {
+        StepVerifier.create(fluxAndMonoGeneratorService.nameMono())
+                .expectNext("alex")
+                .verifyComplete();
+    }
 }
